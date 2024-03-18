9447097ed22420cb67174f633be4eee63dfb8718
Kostya22B <kostuyabondarcyhyk2005@gmail.com>
2024-03-18 04:11:44 +0100

    cosmetic changes for readability

--- js/change-mode.js
+++ js/change-mode.js
@@ -8,3 +8,16 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   }
 })
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    const target = document.querySelector(this.getAttribute('href'));
+    
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
