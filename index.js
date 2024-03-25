a6d5e441835e94c029b3edf2e98b19829040de09
Kostya22B <kostuyabondarcyhyk2005@gmail.com>
2024-03-25 15:50:51 +0100

    new doc

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import Controller from './js/controller.js'
 const root = document.querySelector("#game");
 
 const game = new Game();
-const view = new View(root, 432, 576, 20, 10);
+const view = new View(root, 460, 576, 20, 10);
 const controller = new Controller(game, view);
 
 window.game = game;
