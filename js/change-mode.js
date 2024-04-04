3f7bfbdeb2be4472fc4c4fcb376c6062eb2a24e8
Kostya22B <kostuyabondarcyhyk2005@gmail.com>
2024-04-04 14:28:54 +0200

    small debug

--- js/change-mode.js
+++ js/change-mode.js
@@ -1,6 +1,3 @@
-/**
-* @brief This file is part of Benjamin Schneider. Benjamin Schneider is free software : you can redistribute it and / or modify it under the terms of the GNU General Public License as published by the Free Software Foundation either version 3 of the License or ( at your option ) any later version. Benjamin Schneider is distributed in the hope that it will be useful but WITHOUT ANY WARRANTY ; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
-*/
 document.addEventListener('DOMContentLoaded', function(){
   /**
   * @brief / / object
